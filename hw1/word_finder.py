4a0613939dd49e1f5455d393cd1f44e4eab0230c
Julia Irwin <jairwin06@gmail.com>
2015-02-12 00:38:03 -0500

    updated

--- hw1/word_finder.py
+++ hw1/word_finder.py
@@ -1,11 +1,37 @@
 import sys
 
-#word-finder
+#all_lines = list()
+
+#find lines with the word "my" or "your"
 for line in sys.stdin:
 	line = line.strip()
-	my = line.find("my ")
+	#all_lines.append(line)
+	my = line.find("my")
 	your = line.find("your")
+
 	if my != -1: 
-		print line[my:my+12]
-	if your != -1:
-		print line[your:your+12]
+		mywords = line.split(" ")  #returns a list
+		ind = mywords.index("my")  #returns an int
+		next = mywords.index("my") + 1
+		
+		for i, v in enumerate(mywords):
+			if i == ind:
+				print v
+			if i == next:
+				print v
+			
+
+	if your != -1: 
+		yourwords = line.split(" ")  #returns a list
+		indy = yourwords.index("your")  #returns an int
+		nexty = yourwords.index("your") + 1
+		
+		for i, v in enumerate(yourwords):
+			if i == indy:
+				print v
+			if i == nexty:
+				print v
+
+	
+		
+		
